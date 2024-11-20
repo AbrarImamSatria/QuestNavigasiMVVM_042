014792016d128ed8f2655727f94e74e9526e805d
abrarimamsatria09 <092004abrar@gmail.com>
2024-11-20 21:07:21 +0700

    Menambahkan fungsi Navigasi

--- app/src/main/java/com/example/pertemuan7prak/Navigasi.kt
+++ app/src/main/java/com/example/pertemuan7prak/Navigasi.kt
@@ -1,6 +1,23 @@
 package com.example.pertemuan7prak
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan7prak.ui.viewmodel.MahasiswaViewModel
+
 enum class Halaman{
     Form,
     Data
+}
+
+@Composable
+fun Navigasi(
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+){
+
 }
