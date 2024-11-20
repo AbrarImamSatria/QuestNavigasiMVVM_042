83f67453f5d97d0932d9fc3a25d9889175716700
abrarimamsatria09 <092004abrar@gmail.com>
2024-11-20 20:35:48 +0700

    Codingan dikelas

---
+++ app/src/main/java/com/example/pertemuan7prak/model/ListGender.kt
@@ -0,0 +1,10 @@
+package com.example.pertemuan7prak.model
+
+import com.example.pertemuan7prak.R
+
+object ListGender {
+    val listGender = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
