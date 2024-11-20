c092aa43e348aba52e0ab4d959bc06ac4560d1ac
abrarimamsatria09 <092004abrar@gmail.com>
2024-11-20 22:15:36 +0700

    Memanggil Navigasi agar bisa dijalankan

--- app/src/main/java/com/example/pertemuan7prak/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan7prak/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Pertemuan7PrakTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Navigasi(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
