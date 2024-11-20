f768721f6877e796db441e9fb744df717a902208
abrarimamsatria09 <092004abrar@gmail.com>
2024-11-20 21:38:02 +0700

    Menambahkan navigasi ke halaman Form dengan parameter formul

--- app/src/main/java/com/example/pertemuan7prak/Navigasi.kt
+++ app/src/main/java/com/example/pertemuan7prak/Navigasi.kt
@@ -6,11 +6,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan7prak.model.ListGender
+import com.example.pertemuan7prak.ui.view.FormMahasiswa
 import com.example.pertemuan7prak.ui.viewmodel.MahasiswaViewModel
 
 enum class Halaman{
@@ -30,6 +34,20 @@ fun Navigasi(
             modifier = modifier.padding(isipadding),
             navController = navHost, startDestination = Halaman.Form.name
         ){
+            composable(route = Halaman.Form.name){
+                val konteks = LocalContext.current
+                FormMahasiswa(
+
+                    //Di bawah ini merupakan dari parameter halaman formulir
+                    listGender = ListGender.listGender.map {
+                        isi -> konteks.resources.getString(isi)
+                    },
+                    onSubmitClick = {
+                        viewModel.saveDataMhs(it)
+                        navHost.navigate(Halaman.Data.name)
+                    }
+                )
+            }
 
         }
     }
